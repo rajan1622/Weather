a08f6a4135fb6fb4ce64172157ab86dc80baa4a0
rajan1622 <rajanupatel17@gmaul.com>
2024-08-28 19:05:27 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import SearchFild from "./components/SearchField.vue";
+</script>
+
+<template>
+  <main>
+    <div class="text-center mb-6">
+      {{
+        new Date().toLocaleDateString('en-us', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }}
+    </div>
+    <div> <SearchFild /></div>
+  </main>
+</template>
---
+++ src/components/SearchField.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { reactive} from 'vue';
+
+const searchTerm = reactive({
+  query: '',
+  timeout: null,
+  results: null
+})
+
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout)
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.query !== '') {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/search.json?key=e7370e67c4144ccdafa41843242808&q=${searchTerm.query}`
+      )
+      const data = await res.json()
+      searchTerm.results = data
+
+      console.log(searchTerm);
+    } else {
+      searchTerm.results = null
+    }
+  }, 500)
+}
+
+
+</script>
+
+<template>
+  <div>
+    <!-- search field -->
+    <form>
+      <div class="bg-white border border-indigo-600/30 rounded-lg shadow-lg flex items-center">
+        <i class="fa-solid fa-magnifying-glass p-2 text-indigo-600"></i>
+        <input
+          type="text"
+          placeholder="Search for a place"
+          class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
+          v-model="searchTerm.query"
+          @input="handleSearch"
+          />
+      </div>
+    </form>
+    <!-- search suggestions -->
+    <div class="bg-white my-2 rounded-lg shadow-lg">
+      <div v-for="place in searchTerm.results" :key="place.id">
+          <button
+            @click="getWeather(place.id)"
+            class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left">
+            {{ place.name }}, {{ place.region }}, {{ place.country }}
+
+            
+          </button>
+        </div>
+    </div>
+  </div>
+</template>
